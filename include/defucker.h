66be43c42fc0883898fe41d1ca006eaba984da0d
LionKingSamsher <shk052353@gmail.com>
2022-05-12 22:00:58 +0900

    Update 20220512
    Edit Some File
    	- include/defucker.h : edit and delete methods
    	- src/defucker.cc : add next_function_call()

--- include/defucker.h
+++ include/defucker.h
@@ -2,6 +2,7 @@
 #define __DEFUCKER_H__
 
 #include <iostream>
+#include <map>
 
 #include <stdio.h>
 #include <unistd.h>
@@ -9,11 +10,17 @@
 
 using namespace std;
 
+typedef unsigned long reg_64_t;
+typedef map<string, reg_64_t> Reisters;
+
 class Defucker {
 	pid_t  debug_pid;
 	string debug_prog;
+	Registers registers;
 public:
 	Defucker(const char* debug_prog) : debug_prog(debug_prog) { }
 	void run(const char* argv[]);
+	bool next_syscall();
+	bool next_function_call();
 };
 #endif
--- src/defucker.cc
+++ src/defucker.cc
@@ -7,6 +7,7 @@
 #include <sys/user.h>
 #include <sys/ptrace.h>
 
+/*
 static inline bool trace_next(pid_t pid) {
 	static int stat = 0;
 	ptrace(PTRACE_SYSCALL, pid, 0, 0);
@@ -22,6 +23,17 @@ static inline bool trace_next(pid_t pid) {
 	}
 	return true;
 }
+*/
+
+union hex_dump {
+	unsigned long mem[2];
+	unsigned char bytes[8];
+	/*
+	 * 0 ~ 4 : used
+	 * 5 ~ 7 : reserved
+	 *
+	 * */
+};
 
 static inline void trace_getregs(pid_t pid, struct user_regs_struct* regs) {
 	ptrace(PTRACE_GETREGS, pid, 0, regs);
@@ -55,9 +67,49 @@ void Defucker::run(const char* argv[]) {
 	waitpid(debug_pid, 0, 0);
 
 	cout << "Start Defucker System!\n";
-
-	while(trace_next(debug_pid)){
-		trace_getregs(debug_pid, &regs);
-		trace_print_regs(regs);
-	}
 }	
+
+bool Defucker::next_syscall() {
+	static int stat = 0;
+	ptrace(PTRACE_SYSCALL, pid, 0, 0);
+	waitpid(pid, &stat, 0);
+	
+	if(WIFEXITED(stat)){ // normally turned off
+		int debug_prog_ret = WEXITSTATUS(pid);
+		printf("[%d] Program Done Normally! : %d\n", pid, debug_prog_ret);
+		return false;
+	}
+	if(WIFSIGNALED(stat)){
+		return false;
+	}
+	return true;
+
+}
+
+static inline bool is_call(pid_t pid) {
+	int rip = 0;
+	unsigned char op;
+	union hex_dump hex;
+
+	rip = ptrace(PTRACE_PEEKUSER, pid, 16 * 8, 0); // get rip
+	
+	hex.mem[0] = (unsigned char)ptrace(PTRACE_PEEKTEXT, pid, rip, NULL);
+	hex.mem[1] = (unsigned char)ptrace(PTRACE_PEEKTEXT, pid, rip + sizeof(long), NULL);
+	
+	op = hex.bytes[4];
+
+	if(op == 0xE8) {
+		return true;
+	}
+
+	return false;
+}
+
+bool Defucker::function_call() {
+	static int stat = 0;
+	do{
+		ptrace(PTRACE_SINGLESTEP, debug_pid, 0, 0);
+		waitpid(pid, &stat, 0);
+	}while(is_call(debug_pid));
+
+}
